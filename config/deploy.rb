9233de78614ca405c511b8ce5263c663f3c054c8
vagrant <vagrant@aircrm.dev>
2015-03-17 23:25:39 +0000

    .

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,17 +17,3 @@ namespace :deploy do
     invoke 'unicorn:reload'
   end
 end
-
-namespace :sidekiq do
-  # task :quiet do
-  #   # Horrible hack to get PID without having to use terrible PID files
-  #   puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
-  # end
-  task :restart do
-    execute :sudo, :initctl, :restart, :sidekiq
-  end
-end
-
-after 'deploy:starting', 'sidekiq:quiet'
-after 'deploy:reverted', 'sidekiq:restart'
-after 'deploy:published', 'sidekiq:restart'
