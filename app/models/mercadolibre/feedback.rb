6f4fa4e0a690dc7fab66e56e479dd56de7780253
vagrant <vagrant@aircrm.dev>
2015-03-31 10:37:56 +0000

    feedback refactoring move from feedback worker to feedback.rb

--- app/models/mercadolibre/feedback.rb
+++ app/models/mercadolibre/feedback.rb
@@ -196,7 +196,32 @@ module Mercadolibre
     end
 
 
+    def self.get_meli_feedback(dashboard_id, meli_order_id)
+      if meli_order_id
+        # Fetch Feedbacks from Meli
+        meli_order_feedback  = Meli::Feedback.find_by_order_id(meli_order_id)
 
+        # Update Feedbacks
+        Mercadolibre::Feedback.update_record(:retrieve_item_feedback, dashboard_id, meli_order_id, meli_order_feedback)
+      end
+    end
+
+    def post_seller_feedback(dashboard_id, meli_order_id)
+    dashboard = ::Dashboard.find_by(id: dashboard_id)
+    raise ArgumentError, "Invalid dashboard element.\n dashboard_id=`#{dashboard_id}`\n dashboard=`#{dashboard.inspect}`." unless dashboard.is_a?(::Dashboard)
+      if meli_order_id
+
+        params = {
+          "fulfilled" => true,
+          "rating"    => "positive",
+          "message"   => "Excelente comprador."
+        }
+        Post seller Feedback on Meli
+        meli_order_feedback  = Meli::Feedback.post_feedback(meli_order_id, params)
+
+        puts "\n\n ** Sale Feedback posted: #{meli_order_feedback.inspect}"
+      end
+    end    
 
 
 
