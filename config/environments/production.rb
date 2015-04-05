553ed16377e1fc83aa4ea5e116cbfa15d3f5feb4
vagrant <vagrant@aircrm.dev>
2015-04-05 12:13:00 +0000

    testing asset compile set to false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # config.less.compress = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs.
