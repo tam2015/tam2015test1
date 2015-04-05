7a5f26894b4382dda748b3948e75d8a91e30d70f
vagrant <vagrant@aircrm.dev>
2015-04-05 11:18:21 +0000

    testing redactor rails

--- app/models/redactor_rails/asset.rb
+++ app/models/redactor_rails/asset.rb
@@ -1,17 +1,17 @@
-# class RedactorRails::Asset
-#   include Mongoid::Document
-#   include Mongoid::Timestamps
+class RedactorRails::Asset
+  include Mongoid::Document
+  include Mongoid::Timestamps
 
-#   include RedactorRails::Orm::Mongoid::AssetBase
+  include RedactorRails::Orm::Mongoid::AssetBase
 
-#   field :account_id   , type: Integer
+  field :account_id   , type: Integer
 
-#   delegate :url, :current_path, :size, :content_type, :filename, :to => :data
-#   validates_presence_of :data
+  delegate :url, :current_path, :size, :content_type, :filename, :to => :data
+  validates_presence_of :data
 
-#   before_save :set_account_id
+  before_save :set_account_id
 
-#   def set_account_id
-#     self.account_id= Account.current.id
-#   end
-# end
+  def set_account_id
+    self.account_id= Account.current.id
+  end
+end
