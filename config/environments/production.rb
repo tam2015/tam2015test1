21f9df26f797c31293409443aa7c8289a0de1a55
vagrant <vagrant@aircrm.dev>
2015-04-05 11:44:30 +0000

    testing redactor rails precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,4 +107,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.precompile += ['redactor-rails/*', 'application.css', 'application.js']
+
 end
