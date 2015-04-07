69eedae45aece62a1562a17bf51ba5d934c06e3f
vagrant <vagrant@aircrm.dev>
2015-04-07 17:29:58 +0000

    youtube video implemented

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,8 @@
 module HomeHelper
+
+  def embed(youtube_url)
+    youtube_id = youtube_url.split("=").last
+    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+  end
+
 end
