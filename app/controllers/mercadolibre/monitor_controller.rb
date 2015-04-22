0faa861c96e8a3fffc5461b5f3a33367a4c7f324
vagrant <vagrant@aircrm.dev>
2015-04-22 15:49:04 +0000

    search implemented on monitoring

--- app/controllers/mercadolibre/monitor_controller.rb
+++ app/controllers/mercadolibre/monitor_controller.rb
@@ -4,10 +4,18 @@ class Mercadolibre::MonitorController < ApplicationController
   before_action :set_dashboard, only: [:index]
 
   def monitor
-	refresh_token = current_dashboard.credentials[:refresh_token]
-    Mercadolibre::Item.api.update_token(refresh_token)  	
-    @meli_items = Meli::Item.items_by_category_id params[:category_id]
-    @items = @meli_items.results#.paginate(page: params[:page], per_page: 5)
+    if params[:text_search]
+      refresh_token = current_user.dashboards.first.credentials[:refresh_token]
+      Mercadolibre::Item.api.update_token(refresh_token)      
+      @text_search = Meli::Item.search_by_text params[:text_search]
+      @items = @text_search.results
+      puts "\n\n\n\n\n\n\nitems#{@items.inspect}\n\n\n\n\n\n\n\n"
+    else    
+    	refresh_token = current_dashboard.credentials[:refresh_token]
+      Mercadolibre::Item.api.update_token(refresh_token)  	
+      @meli_items = Meli::Item.items_by_category_id params[:category_id]
+      @items = @meli_items.results#.paginate(page: params[:page], per_page: 5)
+    end
   end
 
   def monitor_from_categories
@@ -19,6 +27,8 @@ class Mercadolibre::MonitorController < ApplicationController
       @categories = Mercadolibre::Category.suggest_categories(params[:query])
     elsif params[:b]
       category_mother_id = Mercadolibre::Category.find_mothers_categories(params[:b])
+    elsif params[:text_search]
+      redirect_to monitor_dashboards_path(text_search: params[:text_search])
     else
       @categories = Mercadolibre::Category.find_all_categories
       @count      = @categories.count
